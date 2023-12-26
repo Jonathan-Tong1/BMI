78674d8af2d8127b21ff848fa061e5a3a9d399d2
Jonathan Tong <jontong321@gmail.com>
2023-12-26 15:06:34 -0500

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by Jonathan on 2023-12-26.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
